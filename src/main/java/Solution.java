82d79f1e1a3e417e6b22f87e90cd6734ecc5d2dc
Ray Romero <raro189@icloud.com>
2023-10-10 13:00:19 -0400

    add Solution for ch 4 & 5

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -6,6 +6,9 @@ public class Solution {
         System.out.println(malesOnly(Person.persons()));
         names(Person.persons()).forEach(System.out::println);
         sortByIncome().forEach(System.out::println);
+        System.out.println("Distinct Genders " + distinctGenders());
+        System.out.println("First 3 people on the list ");
+        firstThreePeople().forEach(System.out::println);
     }
         //Filter the list of person to include only males.
 
@@ -33,6 +36,27 @@ public class Solution {
                 .sorted(Comparator.comparing(Person::getIncome))
                 .toList();
         return sortedList;
+    }
+
+    //Find the disctinct genders in the list of people
+
+    static List<Person.Gender> distinctGenders (){
+        List<Person.Gender> genders = Person.persons()
+                .stream()
+                .map(Person::getGender)
+                .distinct()
+                .toList();
+        return genders;
+    }
+
+    //- Limit the list of persons to the first 3.
+    static List<Person>  firstThreePeople(){
+
+        List<Person> top3 = Person.persons()
+                .stream()
+                .limit(3)
+                .toList();
+        return top3;
     }
 }
 
